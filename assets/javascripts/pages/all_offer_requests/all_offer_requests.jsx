1e29504d6795fbaf375387f867f21ea604df9664
Kamil Lelonek <squixy.sln@gmail.com>
2015-04-23 23:08:59 +0200

    Fetching data for offer requests

--- assets/javascripts/pages/all_offer_requests/all_offer_requests.jsx
+++ assets/javascripts/pages/all_offer_requests/all_offer_requests.jsx
@@ -1,8 +1,17 @@
-import getData from '../../helpers/get_data'
+import getData      from '../../helpers/get_data'
+import OfferRequest from '../../models/offer_request'
 
 export default class AllOfferRequests extends React.Component {
   static fetchData() {
     return getData('/offer_requests');
+  }
+
+  componentWillMount() {
+    this.props.data['offer-requests'].map(
+        offerRequestJsonObject => {
+          return OfferRequest.buildFromJson(offerRequestJsonObject.dump)
+      }
+    )
   }
 
   render() {
