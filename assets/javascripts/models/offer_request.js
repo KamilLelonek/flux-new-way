bb1f796f59848550caebb535721f70a160732631
Kamil Lelonek <squixy.sln@gmail.com>
2015-04-23 20:05:02 +0200

    Created domain models

---
+++ assets/javascripts/models/offer_request.js
@@ -0,0 +1,44 @@
+import Delivery from './delivery'
+import Product  from './product'
+import Company  from './company'
+import Customer from './customer'
+
+export default class OfferRequest {
+  constructor(id, delivery, products, company, customer) {
+    this.id       = id;
+    this.delivery = delivery;
+    this.products = products;
+    this.company  = company;
+    this.customer = customer;
+  }
+
+  static buildFromJson(offerRequestJson) {
+    return new OfferRequest(
+      offerRequestJson['id'],
+      buildDeliveryFromJson(offerRequestJson['delivery']),
+      buildProductsFromJson(offerRequestJson['products']),
+      buildCompanyFromJson(offerRequestJson),
+      buildCustomerFromJson(offerRequestJson)
+    )
+  }
+
+  static buildDeliveryFromJson(deliveryJson) {
+    return Delivery.buildFromJson(deliveryJson)
+  }
+
+  static buildProductsFromJson(productsJson) {
+    return productsJson.map(buildProductFromJson)
+  }
+
+  static buildProductFromJson(productJson) {
+    Product.buildFromJson(productJson)
+  }
+
+  static buildCompanyFromJson(companyJson) {
+    return Company.buildFromJson(companyJson)
+  }
+
+  static buildCustomerFromJson(customerJson) {
+    return Customer.buildFromJson(customerJson)
+  }
+}
---
+++ assets/javascripts/models/product.js
@@ -0,0 +1,21 @@
+import Category from './category'
+
+export default class Product {
+  constructor(id, quantity, category) {
+    this.id       = id;
+    this.quantity = quantity;
+    this.category = category;
+  }
+
+  static buildFromJson(productJson) {
+    return new Product(
+      productJson['id'],
+      productJson['quantity'],
+      buildCategoryFromJson(productJson['category'])
+    )
+  }
+
+  static buildCategoryFromJson(categoryJson) {
+    return Category.buildFromJson(categoryJson)
+  }
+}
