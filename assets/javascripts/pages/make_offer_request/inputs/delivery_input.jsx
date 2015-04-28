82e9bf8405f11ac7e5370a225c79e279bcbc0877
Kamil Lelonek <squixy.sln@gmail.com>
2015-04-28 21:51:28 +0200

    Delivery option provided

--- assets/javascripts/pages/make_offer_request/inputs/delivery_input.jsx
+++ assets/javascripts/pages/make_offer_request/inputs/delivery_input.jsx
@@ -1,8 +1,18 @@
+import { Input } from 'react-bootstrap'
+
+import Delivery from '../../../models/delivery'
+
 export default class DeliveryInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.deliveries = props.deliveries.map(deliveryJson => Delivery.buildFromJson(deliveryJson))
+  }
+
   render() {
     return (
-      <div>
-      </div>
+      <Input type='select' label='Delivery option'>
+        { this.deliveries.map(delivery => <option value={ delivery.id } key={ delivery.name } >{ `${delivery.name}: ${delivery.description}` }</option>) }
+      </Input>
     )
   }
 }
