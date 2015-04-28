e00d83af4078571ea84b3d4611362168d47079c3
Kamil Lelonek <squixy.sln@gmail.com>
2015-04-28 21:20:21 +0200

    Provided customer and company basic component

--- assets/javascripts/pages/make_offer_request/inputs/company_input.jsx
+++ assets/javascripts/pages/make_offer_request/inputs/company_input.jsx
@@ -1,8 +1,23 @@
-export default class CompanyInput extends React.Component {
+import { Row, Col, Input, Glyphicon } from 'react-bootstrap'
+
+export default class extends React.Component {
   render() {
+    const iconName    = <Glyphicon glyph='font' />;
+    const iconPhone   = <Glyphicon glyph='earphone' />;
+    const iconAddress = <Glyphicon glyph='home' />;
+
     return (
-      <div>
-      </div>
+      <Input label='Company details' wrapperClassName='wrapper'>
+        <Row>
+          <Col xs={6}>
+            <Input type='text' addonBefore={ iconName  } placeholder='Company name' />
+          </Col>
+          <Col xs={6}>
+            <Input type='tel' addonBefore={ iconPhone } placeholder='Company phone' />
+          </Col>
+        </Row>
+        <Input type='text' addonBefore={ iconAddress } placeholder='Company address' />
+      </Input>
     )
   }
 }
--- assets/javascripts/pages/make_offer_request/inputs/customer_input.jsx
+++ assets/javascripts/pages/make_offer_request/inputs/customer_input.jsx
@@ -1,8 +1,21 @@
-export default class CustomerInput extends React.Component {
+import { Row, Col, Input, Glyphicon } from 'react-bootstrap'
+
+export default class extends React.Component {
   render() {
+    const iconName  = <Glyphicon glyph='user' />;
+    const iconEmail = <Glyphicon glyph='envelope' />;
+
     return (
-      <div>
-      </div>
+      <Input label='Customer details' wrapperClassName='wrapper'>
+        <Row>
+          <Col xs={6}>
+            <Input type='text' addonBefore={ iconName  } placeholder='Customer name' />
+          </Col>
+          <Col xs={6}>
+            <Input type='text' addonBefore={ iconEmail } placeholder='Customer email' />
+          </Col>
+        </Row>
+      </Input>
     )
   }
 }
