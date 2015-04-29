0c4023c20a0920f34a7089821520de033e96fc14
Kamil Lelonek <squixy.sln@gmail.com>
2015-04-29 15:50:18 +0200

    Integrated add a new product button

--- assets/javascripts/pages/make_offer_request/buttons/add_product_button.jsx
+++ assets/javascripts/pages/make_offer_request/buttons/add_product_button.jsx
@@ -1,9 +1,16 @@
-import { Button } from 'react-bootstrap'
+import { Button, Glyphicon } from 'react-bootstrap'
 
 export default class extends React.Component {
+  addNewProduct() {
+  }
+
   render() {
     return (
-      <Button bsStyle='info'>Submit</Button>
+      <Button bsStyle='info'
+              onClick={ this.addNewProduct.bind(this) }>
+        <Glyphicon glyph='plus' />
+        &nbsp;Add a new product
+      </Button>
     )
   }
 }
--- assets/javascripts/pages/make_offer_request/inputs/products_input.jsx
+++ assets/javascripts/pages/make_offer_request/inputs/products_input.jsx
@@ -2,11 +2,20 @@ import ProductInput from './product_input'
 
 import AddProductButton from '../buttons/add_product_button'
 
+import { Input } from 'react-bootstrap'
+
 export default class ProductsInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { products: [<ProductInput />] }
+  }
+
   render() {
     return (
-      <div>
-      </div>
+      <Input label='Products' wrapperClassName='wrapper'>
+        { this.state.products }
+        <AddProductButton />
+      </Input>
     )
   }
 }
