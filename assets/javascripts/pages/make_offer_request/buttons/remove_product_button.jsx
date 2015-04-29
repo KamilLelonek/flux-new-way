4f379bebba4cf7073a467a5bfee0defee9bbfd68
Kamil Lelonek <squixy.sln@gmail.com>
2015-04-29 17:17:27 +0200

    Simplified removal button

--- assets/javascripts/pages/make_offer_request/buttons/remove_product_button.jsx
+++ assets/javascripts/pages/make_offer_request/buttons/remove_product_button.jsx
@@ -1,9 +1,16 @@
-import { Button } from 'react-bootstrap'
+import { Button, Glyphicon } from 'react-bootstrap'
 
 export default class extends React.Component {
+  removeProduct() {
+
+  }
+
   render() {
     return (
-      <Button bsStyle='warning'>Remove product</Button>
+      <Button bsStyle='warning'
+              onClick={ this.removeProduct.bind(this) } block>
+        <Glyphicon glyph='minus' />
+      </Button>
     )
   }
 }
