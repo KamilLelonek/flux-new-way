23ded081c164df175e29a71075c2e9d63b90c494
Kamil Lelonek <squixy.sln@gmail.com>
2015-04-29 16:53:32 +0200

    Improved deliveries building

--- assets/javascripts/pages/make_offer_request/inputs/delivery_input.jsx
+++ assets/javascripts/pages/make_offer_request/inputs/delivery_input.jsx
@@ -3,18 +3,25 @@ import { Input } from 'react-bootstrap'
 import Delivery from '../../../models/delivery'
 
 export default class DeliveryInput extends React.Component {
-  constructor(props) {
-    super(props);
-    this.deliveries = props.deliveries.map(deliveryJson => Delivery.buildFromJson(deliveryJson))
+  constructor(props, context) {
+    super(props, context);
+    this.buildDeliveries()
+  }
+
+  buildDeliveries() {
+    this.deliveries = this.context.deliveries.map(deliveryJson => {
+      const delivery = Delivery.buildFromJson(deliveryJson);
+      return <option value={ delivery.id } key={ delivery.name } >{ `${delivery.name}: ${delivery.description}` }</option>
+    })
   }
 
   render() {
     return (
       <Input type='select' label='Delivery option'>
-        { this.deliveries.map(delivery => <option value={ delivery.id } key={ delivery.name } >{ `${delivery.name}: ${delivery.description}` }</option>) }
+        { this.deliveries }
       </Input>
     )
   }
 }
 
-DeliveryInput.propTypes = { deliveries: React.PropTypes.array };
+DeliveryInput.contextTypes = { deliveries: React.PropTypes.array };
