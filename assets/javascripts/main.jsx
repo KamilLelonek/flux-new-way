b2501a19a420b1b25a772decea1a29c933e23d72
Kamil Lelonek <squixy.sln@gmail.com>
2015-05-03 12:11:42 +0200

    Keeping one flux instance across the whole application

--- assets/javascripts/main.jsx
+++ assets/javascripts/main.jsx
@@ -8,11 +8,13 @@ import fetchData from './helpers/fetch_data'
 import router    from './router'
 import Flux      from './flux/flux'
 
+const flux = new Flux();
+
 router.run((Handler, state) =>
     fetchData(state.routes)
       .then(data => {
         React.withContext(
-          { flux: new Flux() },
+          { flux: flux },
           () => React.render(<Handler data={ data } />, document.body)
         )
       })
